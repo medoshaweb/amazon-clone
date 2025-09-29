a94b780ef11ce599c017917ec7a93e6231bd95a6
medoshaweb <hailugs@yahoo.com>
2025-09-28 22:33:13 -0700

    render

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-70f44/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/clone-70f44/us-central1/api",
+  baseURL: "https://amazon-api-deploy-n4qd.onrender.com/",
 });
 
 export {axiosInstance}
